abb7668fe8a508a153640012433f29faee9e5a3c
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-02-09 18:31:56 +0000

    modification to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
 	has_many :comments
 	validate_presence_of :poster
 	validate_presence_of :description
-	validate_presence_of :image
+	validate_presence_of :picture
 end
