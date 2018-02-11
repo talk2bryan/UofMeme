523251a0ee59897fbc2ed9c250d8a0d3896c0d69
Riley Siemens <rileysiemens@gmail.com>
2018-02-11 16:33:48 -0600

    Update post_controller.rb

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,7 @@
 class PostController < ApplicationController
   def home
-    @home_title = "U of Meme"
-    @home_header = "Welcome to U of Meme"
+    @home_title = "UofMeme"
+    @home_header = "Welcome to UofMeme"
     @home_message = "Welcome to the UofMeme Home Page. We're under construction."
   end
 end
