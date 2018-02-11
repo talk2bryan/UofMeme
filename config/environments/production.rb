bf1899bcc59ddc7fbdec1b1ebb2d26d333f608b8
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-02-11 23:12:39 +0000

    included a fix for paperclip in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  Paperclip.options.merge!(:command_path => "/usr/bin")
+  Paperclip.options[:command_path] = "/usr/bin/"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
