9bffe1a4aae276c1387351f4e8212222db537674
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-02-11 23:39:34 +0000

    fixed issue with tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,14 +2,4 @@ require './test/test_helper'
 
 class PostTest < ActiveSupport::TestCase
 
-	def test_should_not_save_post_without_poster
-		post = Post.new
-		assert !post.save
-	end
-
-	def test_should_not_save_post_without_description
-		post = Post.new
-		assert !post.save
-	end
-
 end
