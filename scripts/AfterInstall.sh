5478ededa49af0ab50ca5e33fc4227db880d6bac
Bryan Wodi <talk2kamp@gmail.com>
2018-02-20 12:37:33 -0600

    setting up deployment scripts for appspec

---
+++ scripts/AfterInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/deploy/apps/Travis_Deployed_App
+RAILS_ENV=production bundle install --path vendor/bundle
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake assets:clobber
+RAILS_ENV=production bundle exec rake assets:precompile
