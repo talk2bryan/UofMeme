6cbc7ff23a3ea9d0af4caeafed0bc4d7ba49ceec
Bryan Wodi <talk2kamp@gmail.com>
2018-03-05 10:51:22 +0000

    created a new deploy strategy

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -13,13 +13,15 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :deploy_subdir, "#{fetch(:application)}/server"
+#set :deploy_subdir, "#{fetch(:application)}/server"
+set :git_strategy,    RemoteCacheWithProjectRootStrategy
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: ["~/.ssh/id_rsa"] }
+set :project_root,    "server/"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
