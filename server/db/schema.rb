ed14acf49562b133e965fa8f3fb1717e4f73c442
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-03-05 06:09:41 +0000

    Created column for dislike in posts table

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_11_034945) do
+ActiveRecord::Schema.define(version: 2018_03_05_050828) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_02_11_034945) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "dislike", default: 0
   end
 
   create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
