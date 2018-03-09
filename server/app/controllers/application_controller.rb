8c2acc88f1595cff9bab2f38ef71322d62ea17e4
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-03-09 02:00:38 +0000

    made changes

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-	include  ActionController::MimeResponds
     protect_from_forgery with: :exception
     def hello
         render html: "Welcome to the UofMeme Home Page. We're under construction."
