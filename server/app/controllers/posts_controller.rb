24d0fbd2d011101936d32b5366bd3566049a39fb
wontons <antonsawka2@gmail.com>
2018-03-21 17:07:32 +0000

    closer....

--- server/app/controllers/posts_controller.rb
+++ server/app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 		require 'rubygems'
 		require 'RMagick'
 
-	  	@post = Post.new(post_params)
+	  	#@post = Post.new(post_params)
 	  	image = Magick::ImageList.new
 	  	txt = Magick::Draw.new
 
@@ -37,12 +37,14 @@ class PostsController < ApplicationController
 		#paperclip_img = PaperclipModel.new(:paperclip_field => File.new("editedimg.jpeg","r"))
 		#@post.image = img#paperclip_img
 		#@post.image.url = "editedimg.png"
+		@post = Post.new({:poster => "anton", :image => File.open("editedimg.png", "r"), :description => "this is my meme", :user_id => "fucker"})
 
 	    #if params[:post][:uploaded_image_for_io_adapters]
-	      content_type = Mime::Type.lookup(img.to_s).to_s
-	      image_file = Paperclip.io_adapters.for("data:#{content_type};base64,#{params[:post][:uploaded_image_for_io_adapters]}")
-	      image_file.original_filename = params[:post][:file_name]
-	      @post.image = image_file
+	      #content_type = Mime::Type.lookup(img.to_s).to_s
+	      #image_file = Paperclip.io_adapters.for("data:#{content_type};base64,#{params[:post][:uploaded_image_for_io_adapters]}")
+	      #image_file.original_filename = params[:post][:file_name]
+	      #
+	      #@post.image = image_file
 	    #end
 
 	  	if @post.save
