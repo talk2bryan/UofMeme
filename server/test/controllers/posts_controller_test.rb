62d4a139c190815b1c9cfb2c0937f974db7de8db
rileysiemens <rileysiemens@gmail.com>
2018-03-23 21:33:28 +0000

    more tests

--- server/test/controllers/posts_controller_test.rb
+++ server/test/controllers/posts_controller_test.rb
@@ -9,7 +9,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   		sign_in_as @user
 
   		@post = build :post, poster: @user.username, description: "this is a test post"
-  		assert 'p#p_desc_h', "this is a test post"
+  		assert 'p#p_desc_h', :text => "this is a test post"
   	end
 
   	test "user can see other posts" do
@@ -25,4 +25,13 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   		assert 'p#p_desc_h', @posts[1].description
   		assert 'p#p_desc_h', @posts[2].description
   	end
+
+    test "posts have like and dislike buttons" do
+      @user = build :user 
+      sign_in_as @user
+
+      @post = build :post
+      assert 'a.glyphicon glyphicon-thumbs-up glyphicon-align-center'  #thumbs up icon
+      assert 'a.glyphicon glyphicon-thumbs-down glyphicon-align-center'  #thumbs up icon
+    end
 end
