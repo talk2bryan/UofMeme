4b8ef881dc927a7b6d71950fae8c4f4d51f545d5
Ronnie Kwan <crazykwankwan@yahoo.com.hk>
2018-03-23 17:36:38 +0000

    testing user model

--- server/test/models/user_test.rb
+++ server/test/models/user_test.rb
@@ -1,9 +1,17 @@
 require './test/test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	setup do
+		# @validUser's email domain is myumanitoba.ca
+		@validUser = User.new(username: 'memememe', email: 'memememe@myumanitoba.ca', password: 'memememe', password_confirmation: 'memememe')
+		# @validUser2's email domain is umanitoba.ca
+		@validUser2 = User.new(username: 'mickeymouse', email: 'mmouse@umanitoba.ca', password: 'iloveminnie', password_confirmation: 'iloveminnie')
+	end
+
+
 	test 'valid user' do
-		user = Users(:user1)
-		assert user.save
+		assert @validUser.save
+		assert @validUser2.save
 	end
 
 	test 'do not save blank user' do
