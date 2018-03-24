270ca7b7b382f9c37f9ef259c34de75077075136
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-03-24 17:27:37 +0000

    updated the routes so that we can do something like http://api.uofmeme.solutions/v1/users

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   #api
-  namespace :api do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
     namespace :v1 do
       resources :users, only: [:index, :create, :show ]
       resources :posts, only: [:create, :show, :update]
