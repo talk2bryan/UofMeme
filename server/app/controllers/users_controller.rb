182353485025c0f1a8bec5375e94b62c0b6d8c63
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-03-25 18:46:40 +0000

    "Fixed the problem with the show page : If you on that page and you reload it will give you an error Added Ronnie's (forgoten) css style to the show page Created a profile page where the current user can view their posts rerouted the help page to the root url, because it gives an error if clicked on any other page except the home page"

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     def show
    		@user = User.find(params[:id])
-    	redirect_to root_url 
+   		@posts = Post.where(user_id: current_user.id).all
    	end
 
     def create
