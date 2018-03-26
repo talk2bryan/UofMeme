a242eb1336e4979c95f3ffd70a55c0fb82f783fe
Bryan Wodi <talk2kamp@gmail.com>
2018-03-26 06:01:08 -0500

    altered fake data slightly

--- server/spec/factories/posts.rb
+++ server/spec/factories/posts.rb
@@ -1,13 +1,10 @@
-require 'base64'
-
 FactoryBot.define do
   factory :post do
-  	# testing with an image in prod.
     image  { Rack::Test::UploadedFile.new(Rails.root.join('app', 'assets', 'images', 'mememan.jpg'), 'image/jpg') }
     poster { Faker::Lorem.characters(30) }
     description { Faker::Lorem.sentence }
     uploaded_image_for_io_adapters { "This is it" }
     like { 1 }
-    dislike { -1 }
+    dislike { 0 }
   end
 end
