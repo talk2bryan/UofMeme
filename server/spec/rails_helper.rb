6bfa7ce7081d7c416c18b8ec3bf6ac44aea54b01
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2018-03-27 01:42:27 +0000

    system tests for uploading a meme

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -67,6 +67,7 @@ RSpec.configure do |config|
     end
   end
 
+ Capybara.ignore_hidden_elements = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
