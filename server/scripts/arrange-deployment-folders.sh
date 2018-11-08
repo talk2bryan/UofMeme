6e310fc0c2409e06cccc7196989affbe0ac04c64
Bryan Wodi <talk2kamp@gmail.com>
2018-11-08 07:12:19 +0000

    running deployment scripts as passwordless ubuntu user, forgive me

--- server/scripts/arrange-deployment-folders.sh
+++ server/scripts/arrange-deployment-folders.sh
@@ -6,6 +6,6 @@ rm -rf /var/www/UofMeme/prev-deployment
 # Create a backup from current version
 mv /var/www/UofMeme/deployment /var/www/UofMeme/prev-deployment
 
-# Create a deployment folder abd set the owner as nginx
+# Create a deployment folder abd set the owner as ubuntu
 mkdir /var/www/UofMeme/deployment
-chown www-data:www-data /var/www/UofMeme/deployment
+chown ubuntu:ubuntu /var/www/UofMeme/deployment
--- server/scripts/finish-installation.sh
+++ server/scripts/finish-installation.sh
@@ -7,7 +7,7 @@ DEPLOYMENT_TIME=$( date -u "+%Y/%m/%d %H:%M:%S" )
 echo "Deployment finished at: "$DEPLOYMENT_TIME" UTC" > /var/www/UofMeme/deployment/public/deployment_time.txt
 
 # Set permissions and restart nginx
-chown -R www-data:www-data /var/www/UofMeme/deployment
+chown -R ubuntu:ubuntu /var/www/UofMeme/deployment
 chmod -R 755 /var/www/UofMeme/deployment
 
 
