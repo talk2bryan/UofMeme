3da035da4aac5b6003f78069e197061dbf4fddf6
Bryan Wodi <talk2kamp@gmail.com>
2018-11-08 02:53:51 +0000

    setup backup mechanism during deployment

---
+++ server/scripts/arrange-deployment-folders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Remove previous deployment if it exists
+rm -rf /var/www/UofMeme/prev-deployment
+
+# Create a backup from current version
+mv /var/www/UofMeme/deployment /var/www/UofMeme/prev-deployment
+
+# Create a deployment folder abd set the owner as nginx
+mkdir /var/www/UofMeme/deployment
+chown nginx:nginx /var/www/UofMeme/deployment
