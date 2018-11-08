45732502840739eac30bc9e8a0c3a7c33b49f850
Bryan Wodi <talk2kamp@gmail.com>
2018-11-08 05:48:32 +0000

    running sccripts as www-data insteada of nginx

--- server/scripts/arrange-deployment-folders.sh
+++ server/scripts/arrange-deployment-folders.sh
@@ -8,4 +8,4 @@ mv /var/www/UofMeme/deployment /var/www/UofMeme/prev-deployment
 
 # Create a deployment folder abd set the owner as nginx
 mkdir /var/www/UofMeme/deployment
-chown nginx:nginx /var/www/UofMeme/deployment
+chown www-data:www-data /var/www/UofMeme/deployment
