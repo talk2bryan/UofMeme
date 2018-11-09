8fa8a709c8fc46480b96f769277c16161b790135
Bryan Wodi <talk2kamp@gmail.com>
2018-11-09 02:29:03 +0000

    running local bundle exec

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -3,11 +3,6 @@ set -x
 
 cd /var/www/UofMeme/deployment
 
-echo $(which rvm)
-source $(which rvm)
-
-echo $(whoami)
-echo $(pwd)
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
-RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
+RAILS_ENV=production ./bin/bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
