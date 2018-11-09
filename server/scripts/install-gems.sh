08f3669403d3d47eea96582dd932377688d9f3f5
Bryan Wodi <talk2kamp@gmail.com>
2018-11-09 02:13:12 +0000

    added a bunch of debug statements for deployment

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -x
 
 cd /var/www/UofMeme/deployment
 
+echo $(which rvm)
+source $(which rvm)
+
+echo $(whoami)
+echo $(pwd)
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
 RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
