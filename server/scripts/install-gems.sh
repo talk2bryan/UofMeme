6fb6c559f6e4f16a1cd5b33b6f9553545ce002e5
Bryan Wodi <talk2kamp@gmail.com>
2018-11-10 04:56:30 +0000

    source rvm to find bundle

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-cd /var/www/UofMeme/deployment && whoami && pwd && RAILS_ENV=production ./bin/bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
+cd /var/www/UofMeme/deployment && source ~/.rvm/scripts/rvm && RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
 
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
