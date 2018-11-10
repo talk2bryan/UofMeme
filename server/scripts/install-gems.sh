6e0037ff836efc0c139c7482425528acab5feda4
Bryan Wodi <talk2kamp@gmail.com>
2018-11-10 23:17:22 +0000

    codedeploy

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -2,9 +2,8 @@
 set -x
 
 cd /var/www/UofMeme/deployment
-whoami
-
+ls
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
-RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
+RAILS_ENV="production" bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
 
