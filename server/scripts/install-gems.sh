f97cb0f5541602b64f17d3854f9b87fdb4477a51
Bryan Wodi <talk2kamp@gmail.com>
2018-11-10 06:05:51 +0000

    tracing code deploy execution dir

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -x
 
-cd /var/www/UofMeme/deployment && source ~/.rvm/scripts/rvm && RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
-
+cd /var/www/UofMeme/deployment
+touch iamhere.txt
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
+RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
+
