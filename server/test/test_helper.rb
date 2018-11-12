3d275ca9ba70a1448823819606671c90b68e1e86
roland <siroapple10@gmail.com>
2018-11-12 08:21:25 +0000

    fixed indentation for test

--- server/test/test_helper.rb
+++ server/test/test_helper.rb
@@ -1,21 +1,21 @@
-require_relative '../config/environment'
-require 'rails/test_help'
-require 'capybara'
-require 'capybara/dsl'
+  require_relative '../config/environment'
+  require 'rails/test_help'
+  require 'capybara'
+  require 'capybara/dsl'
 
-class ActiveSupport::TestCase
+  class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
 
-module SignInHelper
+  module SignInHelper
   def sign_in_as(user)
     post "/login", params: { session: { email: user.email, password: user.password } }
   end
 end
 
-class ActionDispatch::IntegrationTest
+  class ActionDispatch::IntegrationTest
   include SignInHelper
 end
