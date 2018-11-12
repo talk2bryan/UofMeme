a1639168cb329fe93830bfb0f8d03d4a6fb83b76
Bryan Wodi <talk2kamp@gmail.com>
2018-11-12 01:54:40 +0000

    using correct ruby version for post install

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -3,7 +3,7 @@ set -x
 
 cd /var/www/UofMeme/deployment
 sudo gem install bundler
-
+rvm use 2.4.1
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
 RAILS_ENV=production /var/www/UofMeme/deployment/bin/bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile /var/www/UofMeme/deployment/Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
