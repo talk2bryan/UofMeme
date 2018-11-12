0a9d37544930cc852d1a2acf2c8547620a6fff19
Bryan Wodi <talk2kamp@gmail.com>
2018-11-12 23:00:21 +0000

    proper encryption-decryption for ssh keys for Travis

--- server/scripts/vagrant-install.sh
+++ server/scripts/vagrant-install.sh
@@ -2,7 +2,7 @@
 
 set -x # For debugging
 gem install selenium-webdriver
-openssl aes-256-cbc -K $encrypted_a8f8c3a53860_key -iv $encrypted_a8f8c3a53860_iv -in config/id_rsa.enc -out config/id_rsa -d
+openssl aes-256-cbc -K $encrypted_9f87cb6b292a_key -iv $encrypted_9f87cb6b292a_iv -in config/id_rsa.enc -out config/id_rsa -d
 chmod 600 config/id_rsa
 mv config/id_rsa ~/.ssh/id_rsa
 
