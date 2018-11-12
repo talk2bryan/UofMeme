ce6cd045e23c136d1fec4a0185e8db7d04ece9bd
Bryan Wodi <talk2kamp@gmail.com>
2018-11-12 17:27:37 +0000

    dry run

--- server/scripts/install-gems.sh
+++ server/scripts/install-gems.sh
@@ -4,5 +4,4 @@ set -x
 cd /var/www/UofMeme/deployment/
 # Bundle location: /var/www/UofMeme/shared/bundle
 # Binary location: /var/www/UofMeme/shared/bin
-RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet
-
+su ubuntu -c 'RAILS_ENV=production bundle install --binstubs /var/www/UofMeme/shared/bin --gemfile ./Gemfile --path /var/www/UofMeme/shared/bundle --without development test --deployment --quiet'
