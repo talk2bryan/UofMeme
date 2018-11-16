bf7189a48d7c7a7c8895dac192e41a57f4951aa8
Bryan Wodi <talk2kamp@gmail.com>
2018-11-16 05:57:57 +0000

    added a default url and modeified open brace

--- server/app/models/post.rb
+++ server/app/models/post.rb
@@ -6,14 +6,13 @@ class Post < ApplicationRecord
 	attr_accessor :uploaded_image_for_io_adapters, :file_name, :top_text, :bot_text
 
 	
-	has_attached_file :image, styles: 
-	{   
+	has_attached_file :image, styles: {   
 		original: "1920x1080>",
 		thumb:  "100x100>",
 		medium: "350x400!", 
 		square: "200x200#",
 		large:  "500x550!"
-	},
+	}, default_url: "/images/:style/missing.png"
 	:convert_options => {
     :medium => "-quality 100 -strip"} 
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
