392fb92b418ef2c9ef3b963e4de0654285277909
Bryan Wodi <talk2kamp@gmail.com>
2018-11-16 06:04:27 +0000

    it is the commas that kill you

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -46,9 +46,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: Rails.application.credentials.dig(:aws, :bucket_name) 
-      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id) 
-      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key) 
+      bucket: Rails.application.credentials.dig(:aws, :bucket_name), 
+      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id), 
+      secret_access_key: Rails.application.credentials.dig(:aws,
+      :secret_access_key), 
       s3_region: Rails.application.credentials.dig(:aws, :region) 
     }
   }
