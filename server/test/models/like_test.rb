3b87054b6ba695ae9bf663a1e682c68d582ed765
Bryan Wodi <talk2kamp@gmail.com>
2019-01-05 09:28:27 +0000

    removed irrelevant assertions

--- server/test/models/like_test.rb
+++ server/test/models/like_test.rb
@@ -1,54 +1,30 @@
-require './test/test_helper'
+require 'test_helper'
 
 class LikesTest < ActiveSupport::TestCase
-   def setup 
-      @user = users(:user1)
-     # @post = posts(:valid_post)
+  setup do
+     @user = users(:user1)
      @post = Post.new
      @post.poster =  'mmmmmmmm'
-     @post.image = File.new("test/fixtures/files/test.png")
+     file_path = Rails.root.join('test', 'fixtures', 'files', 'test.png')
+     @post.image = fixture_file_upload(file_path, 'image/png')
      @post.description = "my mememe"
      @post.user_id = @user.id
-     assert @post.save
-   end
-
-   test 'Testing null like input' do
-         like = Like.new
-         assert_not like.save
-   end
-
-   test 'Testing valid like input' do
-         like = Like.new(user_id: @user.id, post_id: @post.id)
-         assert like.save
-   end
-
-   test 'Testing invalid post_id input' do
-        like = Like.new(user_id: @user.id)
-        assert_not like.save
-
-        like = Like.new(user_id: @user.id, post_id: 123)
-        assert_not like.save
-
-        like = Like.new(user_id: @user.id, post_id: "")
-        assert_not like.save
-
-   end
-
-   test 'Testing invalid user_id input' do
-        like = Like.new(post_id: @post.id)
-        assert_not like.save
-
-        like = Like.new(user_id: 123, post_id: @post.id)
-        assert_not like.save
-
-        like = Like.new(user_id: "", post_id: @post.id)
-   end
-
-   test 'should report error' do
-        assert_raises(NameError) do
-            some_undefined_variable
-        end
-   end
-
+     assert @post.save!
+  end
   
-end  
+  test 'should not save like without user and post id' do
+    like = Like.new
+    assert_not like.save
+  end
+
+  test 'should save like with valid params' do
+    like = Like.new(user_id: @user.id, post_id: @post.id)
+    assert like.save!
+  end
+
+ test 'should report error' do
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
+end
