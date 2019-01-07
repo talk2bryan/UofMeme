588c30b02de45a2d5c6953c9aeaab8797d869705
Bryan Wodi <talk2kamp@gmail.com>
2019-01-07 07:10:33 +0000

    fixed errors and readability

--- server/test/models/post_test.rb
+++ server/test/models/post_test.rb
@@ -2,14 +2,15 @@ require './test/test_helper'
 
 class PostTest < ActiveSupport::TestCase
 	def setup
-		@testImage = File.new("test/fixtures/files/test.png")
-
+		file_path = Rails.root.join('test', 'fixtures', 'files', 'test.png')
+    @testImage = fixture_file_upload(file_path, 'image/png')
 		@user = users(:user1)
-		@validPost = Post.new(poster: @user.username, image: @testImage, description: 'I love meme', user_id: @user.id)
+		@validPost = Post.new(poster: @user.username, image: @testImage,
+    description: 'I love meme', user_id: @user.id)
 	end
 
 	test 'valid post' do
-		assert @validPost.save
+		assert @validPost.save!
 	end
 
 	test 'invalid poster' do
@@ -17,54 +18,63 @@ class PostTest < ActiveSupport::TestCase
 		assert_not post.save, 'Poster is missing'
 		post = Post.new(poster: '' , description: 'I love meme', user_id: @user.id)
 		assert_not post.save, 'Poster cannot be empty string'
-		post = Post.new(poster: 'mememe', description: 'I love meme', user_id: @user.id)
+	  post = Post.new(poster: 'mememe', description: 'I love meme', user_id:
+    @user.id)
 		assert_not post.save, 'Poster does not exist'
 	end
 
 	test 'invalid image' do
-		post = Post.new(poster: @user.username, description: 'I love meme', user_id: @user.id)
+		post = Post.new(poster: @user.username, description: 'I love meme', user_id:
+    @user.id)
 		assert_not post.save, 'Image is missing'
-		post = Post.new(poster: @user.username, image: '', description: 'I love meme', user_id: @user.id)
-		assert_not post.save, 'Image cannot not be empty string'
 
 		# Image is not found
 		assert_raises(SystemCallError) do
-			testImage = File.new("test/fixtures/files/memememe.jpg")
-			post = Post.new(poster: @user.username, image: testImage, description: 'I love meme', user_id: @user.id)
+		  file_path = Rails.root.join('test', 'fixtures', 'files', 'notexists.png')
+			testImage = File.new(file_path)
+			post = Post.new(poster: @user.username, image: testImage, description: 'I
+      love memes', user_id: @user.id)
 		end
 		assert_not post.save, 'Image is not found'
 
 		# Image path is invalid
 		assert_raises(SystemCallError) do
 			testImage = File.new("adsfasdf")
-			post = Post.new(poster: @user.username, image: testImage, description: 'I love meme', user_id: @user.id)
+			post = Post.new(poster: @user.username, image: testImage, description: 'I
+      love memes', user_id: @user.id)
 		end
 		assert_not post.save, 'Image path is invalid'
 
 		# Image path is empty
 		assert_raises(SystemCallError) do
 			testImage = File.new("")
-			post = Post.new(poster: @user.username, image: testImage, description: 'I love meme', user_id: @user.id)
+      post = Post.new(poster: @user.username, image: testImage, description: 'I
+      love memes', user_id: @user.id)
 		end
 		assert_not post.save, 'image path is empty'
 	end
 
 	test 'invalid description' do
-		post = Post.new(poster: @user.username, image: @testImage, user_id: @user.id)
+		post = Post.new(poster: @user.username, image: @testImage, user_id:
+    @user.id)
 		assert_not post.save, 'description is missing'
 
-		post = Post.new(poster: @user.username, image: @testImage, description: '', user_id: @user.id)
+		post = Post.new(poster: @user.username, image: @testImage, description: '',
+    user_id: @user.id)
 		assert_not post.save, 'description is empty string'
 	end
 
 	test 'invalid user_id' do
-		post = Post.new(poster: @user.username, image: @testImage, description: 'I love meme')
+		post = Post.new(poster: @user.username, image: @testImage, description: 'I
+    love memes')
 		assert_not post.save, 'user_id is missing'
 
-		post = Post.new(poster: @user.username, image: @testImage, description: 'I love meme', user_id: '')
+		post = Post.new(poster: @user.username, image: @testImage, description: 'I
+    love memes', user_id: '')
 		assert_not post.save, 'user_id cannot be empty string'
 
-		post = Post.new(poster: @user.username, image: @testImage, description: 'I love meme', user_id: 'IAMTRUMP')
+		post = Post.new(poster: @user.username, image: @testImage, description: 'I
+    love memes', user_id: 'IAMTRUMP')
 		assert_not post.save, 'user_id does not exist'
 	end
 
