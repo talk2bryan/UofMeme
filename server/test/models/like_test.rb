27d2bd3685126b81beeaeba4305b4c76455f6ddc
Bryan Wodi <talk2kamp@gmail.com>
2019-01-07 03:42:09 +0000

    slight readability improvements

--- server/test/models/like_test.rb
+++ server/test/models/like_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class LikesTest < ActiveSupport::TestCase
   setup do
-     @user = users(:user1)
-     @post = Post.new
-     @post.poster =  'mmmmmmmm'
-     file_path = Rails.root.join('test', 'fixtures', 'files', 'test.png')
-     @post.image = fixture_file_upload(file_path, 'image/png')
-     @post.description = "my mememe"
-     @post.user_id = @user.id
-     assert @post.save!
+    @user = users(:user1)
+    @post = Post.new
+    @post.poster =  'mmmmmmmm'
+    file_path = Rails.root.join('test', 'fixtures', 'files', 'test.png')
+    @post.image = fixture_file_upload(file_path, 'image/png')
+    @post.description = "my mememe"
+    @post.user_id = @user.id
+    assert @post.save!
   end
   
   test 'should not save like without user and post id' do
@@ -21,8 +21,7 @@ class LikesTest < ActiveSupport::TestCase
     like = Like.new(user_id: @user.id, post_id: @post.id)
     assert like.save!
   end
-
- test 'should report error' do
+  test 'should report error' do
     assert_raises(NameError) do
       some_undefined_variable
     end
