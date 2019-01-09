75c0b30eefd4a23b54d08fff7a86b56b5bf1805b
roland <siroapple10@gmail.com>
2019-01-09 07:17:26 +0000

    fixed syntax error

--- server/spec/controllers/users_controller_spec.rb
+++ server/spec/controllers/users_controller_spec.rb
@@ -2,30 +2,30 @@
 
  RSpec.describe UsersController, type:: controller do
 
-     describe "GET #new"
-   do
-     it "returns http success"
-   do
+     describe "GET #new" do
+   
+     it "returns http success" do
+   
      get: new
    expect(response).to be_successful
  expect(response).to have_http_status(200)
  end
  end
 
- describe "GET #index"
- do
-   it "returns http success"
- do
+ describe "GET #index" do
+ 
+   it "returns http success" do
+ 
    get: index
  expect(response).to be_successful
  expect(response).to have_http_status(200)
  end
  end
 
- describe "POST #create"
- do
-   it "redirects to the root url, indicating success"
- do
+ describe "POST #create" do
+ 
+   it "redirects to the root url, indicating success" do
+ 
    params = {
      user: {username: "John",email: "John@myumanitoba.ca",password: "12345678"
      ,password_confirmation: "12345678"
@@ -35,8 +35,8 @@
  expect(response).to redirect_to root_url
  end
 
- it "redirects to re-render the sign up page, indicating failure"
- do
+ it "redirects to re-render the sign up page, indicating failure" do
+ 
    params = {
      user: {
        username: "John",email: "John@myumanitoba.ca",password: "1245678"
@@ -46,17 +46,17 @@
  post: create, params: params
  expect(response).to be_successful
  expect(response).to have_http_status(200)
- end
- end
+end
+end
 
- describe "POST #new"
- do
-   it "returns http success"
- do
+ describe "POST #new" do
+ 
+   it "returns http success"do
+ 
    post: new
  expect(response).to be_successful
  expect(response).to have_http_status(200)
- end
- end
+end
+end
 
- end
+end
