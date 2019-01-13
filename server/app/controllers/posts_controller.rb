cb7cd4bcc717c00165da737982d46613a831a027
Bryan Wodi <talk2kamp@gmail.com>
2019-01-13 14:34:11 +0000

    successfully create a meme with user params

--- server/app/controllers/posts_controller.rb
+++ server/app/controllers/posts_controller.rb
@@ -13,68 +13,51 @@ class PostsController < ApplicationController
    	end
 
 	def create
-	  	@post = Post.new(post_params)
-
-	  	toptxt = params[:post][:top_text]
+	  @post = Post.new(post_params)
+    toptxt = params[:post][:top_text]
  		bottxt = params[:post][:bot_text]
-
-
+        
  		if post_params[:image].present?
+      unless @post.image.content_type.starts_with?('image/gif')
+        unless params[:post][:top_text].blank? && params[:post][:top_text].blank?
+          top_txt = params[:post][:top_text]
+          bot_txt = params[:post][:bot_text]
 
- 		unless toptxt.blank? && bottxt.blank?
-		  	uploaded_image = Magick::ImageList.new(post_params[:image].path)[0]
-		  	temp = Tempfile.new("tmp")
-		  	width = uploaded_image.columns
-		  	height = uploaded_image.rows/3
+          if Rails.env.production?
+            temp_img = MiniMagick::Image.open(url_for(@post.image))
+          else
+            temp_img =
+            MiniMagick::Image.open(ActiveStorage::Blob.service.send(:path_for,
+            @post.image.key))
+          end
 
-=begin
-		  	unless toptxt.blank?
-			  	top_img = Magick::Image.read("caption:#{toptxt}") {
-				 	self.fill = '#FFFFFF'
-				 	self.stroke = '#000000'
-				 	self.stroke_width = 0.1
-				 	self.pointsize = width/10
-				 	self.size = "#{width}x#{height}"
-				 	self.background_color = "none"				  
-				}.first
-				uploaded_image = uploaded_image.composite(top_img, Magick::NorthGravity, Magick::OverCompositeOp)
-			end
+          temp_img.combine_options do |c|
+            c.gravity 'North'
+            c.pointsize '35'
+            c.draw "text 0, 0 '#{top_txt}'"
+            c.fill('#000000')
+            c.gravity 'South'
+            c.draw "text 0, 0 '#{bot_txt}'"
+            c.fill('#000000')
+            c.font 'Helvetica'
+          end
+          ext = File.extname(@post.image.filename.to_s)
+          temp_img_filename = "_editedimage_#{ext}"
+          temp_img.write(temp_img_filename)
 
-			unless bottxt.blank?
-				bot_img = Magick::Image.read("caption:#{bottxt}") {
-					self.fill = '#FFFFFF'
-				 	self.stroke = '#000000'
-					self.stroke_width = 0.01
-					self.pointsize = width/10
-					self.size = "#{width}x#{height}"
-					self.background_color = "none"
-				}.first
-				uploaded_image = uploaded_image.composite(bot_img, Magick::SouthGravity, Magick::OverCompositeOp)
-		  	end
-=end
+          @post.image.attach(io: File.open(temp_img_filename), filename:
+          @post.image.filename, content_type: @post.image.content_type)
+          File.delete(temp_img_filename)
+        end
+      end
+    end
 
-			uploaded_image.format = "png"
-			uploaded_image.write("_editedimage_.png")
-			temp = File.open("_editedimage_.png", "r")	
- 			@post = Post.new({:poster => post_params[:poster], :image => temp, :description => post_params[:description], :user_id => post_params[:user_id]})
- 		end
-
- 	end
-
-	  	if @post.save
-	  		flash[:info] = "Meme successfully created"
-	     	redirect_to root_url
-	  	else
-	  		flash[:info] = "There was an error. Your Meme wasn't created"
-	  		render "new"
-	  	end
-	  	
-	  	if post_params[:image].present?
-
-	  	if !toptxt.blank? || !bottxt.blank?
-	  		File.delete("_editedimage_.png")
-	  		temp.close
-	  	end
+	  if @post.save
+	    flash[:info] = "Meme successfully created"
+	    redirect_to root_url
+	  else
+	    flash[:info] = "There was an error. Your Meme wasn't created"
+	  	render "new"
 	  end
 	end
 
