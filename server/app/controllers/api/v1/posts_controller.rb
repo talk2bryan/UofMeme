b593c419ea77a16cce9f742fb978456bfa5f9c4d
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2019-02-10 05:13:50 +0000

    posts api functionality and tests

--- server/app/controllers/api/v1/posts_controller.rb
+++ server/app/controllers/api/v1/posts_controller.rb
@@ -16,26 +16,29 @@ class Api::V1::PostsController < Api::V1::BaseController
 	def create
 		@post = Post.new(post_params)
 
-		if params[:post][:uploaded_image_for_io_adapters]
-			content_type = Mime::Type.lookup_by_extension(File.extname(params[:post][:file_name])[1..-1]).to_s
+		supported_data_type = ["image/png", "image/jpg", "image/jpeg", "image/gif"]
+		image_data_type = data_type.to_s
+
+		if params[:post][:uploaded_image_for_io_adapters] && params[:post][:file_name] && (supported_data_type.include?(image_data_type))
+			content_type = Mime::Type.lookup_by_extension(File.extname(image_name)[1..-1]).to_s
 
 			@post.image.attach(
 				io: image_io,
 				filename: image_name,
-				content_type: content_type
+				content_type: image_data_type
 			)
 		end
 
-		 if @post.save
+		if @post.save
 			url = url_for(@post.image)
 			@post.uploaded_image_for_io_adapters = url
 			
 			render(
-				json: Api::V1::PostSerializer.new(@post).to_json,
+				json: {post: Api::V1::PostSerializer.new(@post)}.to_json,
 				status: 201
 			)
 	    else
-	        render json: @post.errors, status: :unprocessable_entity
+	        render json: {errors: @post.errors}.to_json, status: :unprocessable_entity
 	    end
 	end
 
@@ -45,12 +48,21 @@ class Api::V1::PostsController < Api::V1::BaseController
 		params.require(:post).permit(:poster, :description, :user_id, :file_name, :uploaded_image_for_io_adapters)
 	end
 
+	def decoded_image
+		encoded_image = params[:post][:uploaded_image_for_io_adapters]
+		decoded_image = encoded_image ? Base64.decode64(encoded_image) : ""
+	end
+
 	def image_io
-		decoded_image= Base64.decode64(params[:post][:uploaded_image_for_io_adapters])
 		StringIO.new(decoded_image)
 	end
 
 	def image_name
 		params[:post][:file_name]
+	end
+
+	def data_type
+		require 'mimemagic'
+		MimeMagic.by_magic(decoded_image)
 	end
 end
