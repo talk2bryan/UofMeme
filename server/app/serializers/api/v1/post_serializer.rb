50f9a24e8bdf82912a5b37f1aa68b408f8eb2cb7
Ehidiamen Osarhemen <osarheme@myumanitoba.ca>
2019-02-18 03:59:21 +0000

    likes and dislikes api functionality

--- server/app/serializers/api/v1/post_serializer.rb
+++ server/app/serializers/api/v1/post_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostSerializer < Api::V1::BaseSerializer
 
-	attributes :poster, :description, :user_id, :file_name, :uploaded_image_for_io_adapters, :like, :dislike
+	attributes :id, :poster, :description, :user_id, :file_name, :uploaded_image_for_io_adapters, :like, :dislike
 	
 	def created_at
 		object.created_at.in_time_zone.iso8601 if object.created_at
