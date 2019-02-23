c6392404d3f8f17fdd2f95abc8dadf647d140910
roland <siroapple10@gmail.com>
2019-02-23 20:20:46 +0000

    fixed bug for failed build

--- server/app/controllers/api/v1/base_controller.rb
+++ server/app/controllers/api/v1/base_controller.rb
@@ -32,14 +32,12 @@ end
  end
 
  def authenticate_user!
- token, options = ActionController::HttpAuthentication::Token.token_and_options
- (request)
+ token, options = ActionController::HttpAuthentication::Token.token_and_options(request)
 
  user_email = options.blank?? nil : options[:email]
  user = user_email && User.find_by(email: user_email)
 
- if user && ActiveSupport::SecurityUtils.secure_compare(user.authentication_
- token, token)
+ if user && ActiveSupport::SecurityUtils.secure_compare(user.authentication_token, token)
  @current_user = user
  else
  return unauthenticated!
