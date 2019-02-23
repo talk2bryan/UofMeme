f01ddac60d41b2ede4e51bee92d98f49047c4659
Anton <antonsawka2@gmail.com>
2019-02-23 04:26:48 +0000

    user tests (incomplete)

--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -1,7 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  #change to factorybot
   @user = User.create(username: "john", email: "john@umanitoba.ca", password:  "password", password_confirmation: "password")
+
+  describe "username" do
+    #could be create instead
+    #change to factorybot
+    subject { User.new(username: "frank", email: "frank@umanitoba.ca", password:  "password", password_confirmation: "password") }
+
+    it "is a valid username" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without username" do
+      subject.username = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with short username" do
+      subject.username = Faker::Lorem.characters(2)
+      # subject.username = "Jo"
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with long username" do
+      subject.username = Faker::Lorem.characters(31)
+      # subject.username = "frankfrankfrankfrankfrankfrank1"
+      expect(subject).to_not be_valid
+    end
+
+    #could fail due to using new instead of create
+    it "is not valid without unique username" do
+      subject.username = "john"
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe "email" do
+    subject { User.new(username: "frank", email: "frank@umanitoba.ca", password:  "password", password_confirmation: "password") }
+
+    it "is a valid email" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with incorrect regex" do
+      subject.email = Faker::Lorem.characters(10) + "@gmail.ca"
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with long email" do
+      subject.email = Faker::Lorem.characters(255) + "@umanitoba.ca"
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without unique email" do
+      subject.email = "john@umanitoba.ca"
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe "password" do
+    subject { User.new(username: "frank", email: "frank@umanitoba.ca", password:  "password", password_confirmation: "password") }
+
+    it "" do
+
+    end
+  end
+
   describe "associations" do
     it { should have_many(:posts) }
     it { should have_many(:comments) }
@@ -34,4 +105,6 @@ RSpec.describe User, type: :model do
     it { should_not allow_value("john@myumanitoba.com").for(:email) }
     it { should_not allow_value("john@hotmail.com").for(:email) }
   end
+
+
 end
