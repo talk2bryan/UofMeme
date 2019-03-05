98280a7482b272ee9ffd4e88ccbd6191cd9ed2b2
roland <siroapple10@gmail.com>
2019-03-05 04:20:55 +0000

    corrected migration for commennt

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_233822) do
+ActiveRecord::Schema.define(version: 2019_03_05_030218) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,17 +31,6 @@ ActiveRecord::Schema.define(version: 2018_12_24_233822) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "commenter", limit: 255, null: false
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "post_id"
-    t.bigint "user_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "dislikes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -92,8 +81,6 @@ ActiveRecord::Schema.define(version: 2018_12_24_233822) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "dislikes", "posts"
   add_foreign_key "dislikes", "users"
   add_foreign_key "likes", "posts"
