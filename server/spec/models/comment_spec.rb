4ba0979c37d7e69eb31a53027bf80b6801d08c99
roland <siroapple10@gmail.com>
2019-03-11 10:08:20 +0000

    comment test

--- server/spec/models/comment_spec.rb
+++ server/spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new}
+
+  it "is valid with valid attributes" do
+    subject.comment_id=1
+    subject.post_id=3
+    subject.user_id=2
+    subject.commenter="Anything"
+    subject.comment="Anything"
+
+    subject.timeAttribute=DateTime.now
+    expect(subject).to be_valid
+  end
+  it "is not valid without a commenter" do
+
+    expect(subject).to_not be_valid
+  end
+
+
+
 end
